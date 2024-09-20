9176d88361f464d13ba1097b9de8c993a29ec2f8
Edward <nintenderboy@gmail.com>
2024-09-20 07:37:30 -0700

    Change WebGuest URL to actual

--- utils/urls.py
+++ utils/urls.py
@@ -1,3 +1,3 @@
 # -- DEMOSTAND --
 # Pages
-WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/SMbWeqw7yjDa31NF"
+WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/bB3Itnb7Rq0E6wGI"
