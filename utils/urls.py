27a37747bad37e278b89c233abd9aff59cd7757c
Edward <nintenderboy@gmail.com>
2024-11-07 05:52:38 -0800

    Migration from github

--- utils/urls.py
+++ utils/urls.py
@@ -1,3 +1,3 @@
 # -- DEMOSTAND --
 # Pages
-WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/5M8BYs5uDkeAb6xJ"
+WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/AvAko8V3sQssVARw"
