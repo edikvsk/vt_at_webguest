b113e7d92ec2caa1de76faf00e6899392d8713b3
Edwardv <edwardv@medialooks.com>
2024-11-08 13:21:22 +0200

    change urls

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 USERNAME = "AT_guest"
 LOCATION = "Web"
 
-CHROME_DRIVER_PATH = "C:/Users/Demo/Documents/chromedriver/chromedriver.exe"
+CHROME_DRIVER_PATH = "D:/chromedriver/chromedriver.exe"
 
 LOGS_DIR = "C:/Users/Demo/PycharmProjects/VT WebGuest/VT WebGuest AT/logs"
--- utils/urls.py
+++ utils/urls.py
@@ -1,3 +1,3 @@
 # -- DEMOSTAND --
 # Pages
-WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/AvAko8V3sQssVARw"
+WEB_GUEST_PAGE_URL = "https://vt08.medialooks.com:8080/wg2/3ZQh3QRFj7Xo@k8y"
