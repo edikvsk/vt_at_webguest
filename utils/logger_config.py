41c8db9acb75c41fb6e6a6b03081e7075959d3ae
Edwardv <edwardv@medialooks.com>
2024-11-14 15:04:55 +0200

    add custom error filter

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -2,10 +2,22 @@ import logging
 import sys
 
 
+class CustomErrorFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelname == "ERROR":
+            record.levelname = "ERROR__"
+        return True
+
+
 def setup_logger(test_name):
     logger = logging.getLogger(test_name)
     logger.setLevel(logging.INFO)
+
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(logging.Formatter('\n%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+    # Добавляем фильтр для изменения уровня логирования
+    handler.addFilter(CustomErrorFilter())
+
     logger.addHandler(handler)
     return logger
