a3c04ccf1f9df8d90702d446f98c51ab3887fc2a
Edwardv <edwardv@medialooks.com>
2024-11-22 11:40:06 +0200

    add test framerate

---
+++ test/wg_settings/framerate/test_framerate_60fps.py
@@ -0,0 +1,80 @@
+import os
+import time
+
+import pytest
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+from pages.base_page import BasePage
+from pages.desktop_app_page import DesktopAppPage
+from pages.web_guest_page import WebGuestPage
+from utils.conftest import driver, login_fixture
+from utils.desktop_app import DesktopApp
+from utils.helpers import log_step
+from utils.logger_config import setup_logger
+from utils.urls import PROCESS_PATH
+
+
+@pytest.fixture(scope="function")
+def logger(caplog):
+    test_name = os.path.splitext(os.path.basename(__file__))[0]
+    logger = setup_logger(test_name)
+    return logger
+
+
+@pytest.mark.usefixtures("login_fixture")
+def test_framerate_15fps(driver, logger):
+    wg_page = WebGuestPage(driver)
+    base_page = BasePage(driver)
+    desktop_app = DesktopApp(PROCESS_PATH)
+    desktop_app_page = DesktopAppPage(desktop_app.main_window)
+
+    vt_web_guest_source_name = "Web Guest"
+    framerate = "60 FPS"
+
+    @log_step(logger, "ШАГ 1. Проверка отображения кнопки SETTINGS")
+    def check_settings_button():
+        assert base_page.is_element_present(wg_page.SETTINGS_BUTTON), "Кнопка SETTINGS не отображается"
+
+    @log_step(logger, "ШАГ 2. Нажатие кнопки SETTINGS")
+    def click_settings_button():
+        base_page.click(wg_page.SETTINGS_BUTTON)
+
+    @log_step(logger, "ШАГ 3. Выбор Framerate")
+    def select_framerate():
+        time.sleep(3.5)
+        wg_page.select_framerate(framerate)
+        base_page.click(wg_page.COMBOBOX_BACK_BUTTON)
+        expected_value = framerate
+        actual_value = wg_page.get_settings_item_value_text(wg_page.FRAMERATE_VALUE)
+        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+
+    @log_step(logger, "ШАГ 4. Проверка значения Framerate в VT WebGuest Settings")
+    def check_framerate_field_value_vt():
+        desktop_app_page.right_click_vt_source_item(vt_web_guest_source_name)
+        desktop_app_page.click_vt_source_item(DesktopAppPage.VT_WEB_GUEST_SETTINGS)
+        desktop_app_page.select_combobox_item_by_index(1, 2)
+        desktop_app_page.click_button_by_name(DesktopAppPage.VT_OK_BUTTON)
+
+    @log_step(logger, "ШАГ 5. Проверка значения поля Framerate")
+    def check_framerate_field_value():
+        expected_value = framerate
+        actual_value = wg_page.get_settings_item_value_text(wg_page.FRAMERATE_VALUE)
+        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+
+    try:
+        check_settings_button()
+        click_settings_button()
+        select_framerate()
+        check_framerate_field_value_vt()
+        check_framerate_field_value()
+
+    except (NoSuchElementException, TimeoutException) as e:
+
+        logger.error(f"Ошибка при выполнении теста: {e}")
+
+        pytest.fail(f"Ошибка при выполнении теста: {e}")
+
+    # При необходимости закрыть VT
+    # finally:
+
+    # desktop_app.close_application()
