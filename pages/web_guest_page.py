76e2307e66ceacae3ede543e5adbf690fc8c6b62
Edwardv <edwardv@medialooks.com>
2024-11-25 11:58:16 +0200

    added test audio bitrate

--- pages/web_guest_page.py
+++ pages/web_guest_page.py
@@ -41,6 +41,9 @@ class WebGuestPage(BasePage):
                                     ")='Frame Rate']")
     FRAMERATE_VALUE = (By.XPATH, "//div[@data-cy='frameRate']//span[contains(text(), 'FPS')]")
     COMBOBOX_BACK_BUTTON = (By.XPATH, "//div[@class='mr-1']")
+    AUDIO_BITRATE_COMBOBOX = (By.XPATH, "//div[@class='d-flex flex-grow-1 flex-shrink-1 align-items-center']//span["
+                                        "text("")='Audio Bitrate']")
+    AUDIO_BITRATE_VALUE = (By.XPATH, "//div[@data-cy='audioBitrate']")
 
     # Методы:
     def get_username(self):
@@ -183,3 +186,32 @@ class WebGuestPage(BasePage):
 
         except Exception as e:
             raise RuntimeError(f"Ошибка при выборе Framerate '{framerate_text}': {e}")
+
+    def select_audio_bitrate(self, audio_bitrate_text):
+        """Выбирает Framerate из выпадающего списка по заданному тексту."""
+        try:
+            # Ожидаем, пока комбобокс станет кликабельным и кликаем на него
+            audio_bitrate_combobox = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(self.AUDIO_BITRATE_COMBOBOX)
+            )
+            audio_bitrate_combobox.click()  # Открываем выпадающий список
+
+            # Генерируем правильный текст с символом
+            audio_bitrate_text = audio_bitrate_text.replace("AUDIO BITRATE\n6K", "6k")
+
+            # Ожидаем, пока элемент с нужным Audio Bitrate станет видимым
+            audio_bitrate_option_locator = (
+                By.XPATH, f"//span[contains(@class, 'menu-item-title') and text()='{audio_bitrate_text}']")
+
+            audio_bitrate_option = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(audio_bitrate_option_locator)
+            )
+
+            # Ожидаем, пока элемент станет кликабельным и кликаем на него
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(audio_bitrate_option)
+            ).click()
+
+        except Exception as e:
+            raise RuntimeError(f"Ошибка при выборе Audio Bitrate '{audio_bitrate_text}': {e}")
+
