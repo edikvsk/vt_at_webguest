a7fca562f2d08898e9430fc8abc190749c760be0
Edwardv <edwardv@medialooks.com>
2024-12-02 16:43:35 +0200

    added test video encoder H264

---
+++ test/wg_settings/video_encoder/test_video_encoder_h264.py
@@ -0,0 +1,91 @@
+import os
+import time
+
+import pytest
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+from pages.base_page import BasePage
+from pages.desktop_app_page import DesktopAppPage
+from pages.web_guest_page import WebGuestPage
+from utils.conftest import driver, login_fixture
+from utils.desktop_app import DesktopApp
+from utils.helpers import log_step
+from utils.logger_config import setup_logger
+from utils.stream_handler import StreamHandler
+from utils.urls import PROCESS_PATH
+
+
+@pytest.fixture(scope="function")
+def logger(caplog):
+    test_name = os.path.splitext(os.path.basename(__file__))[0]
+    logger = setup_logger(test_name)
+    return logger
+
+
+@pytest.mark.usefixtures("login_fixture")
+def test_video_encoder_h264(driver, logger):
+    wg_page = WebGuestPage(driver)
+    base_page = BasePage(driver)
+    stream_handler = StreamHandler(driver)
+    desktop_app = DesktopApp(PROCESS_PATH)
+    desktop_app_page = DesktopAppPage(desktop_app.main_window)
+
+    vt_web_guest_source_name = "Web Guest"
+    video_encoder_value = "H264"
+    webrtc_video_encoder_for_h264 = "COT01_102_level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f"
+
+    @log_step(logger, "ШАГ 1. Проверка отображения кнопки SETTINGS")
+    def check_settings_button():
+        assert base_page.is_element_present(wg_page.SETTINGS_BUTTON), "Кнопка SETTINGS не отображается"
+
+    @log_step(logger, "ШАГ 2. Нажатие кнопки SETTINGS")
+    def click_settings_button():
+        base_page.click(wg_page.SETTINGS_BUTTON)
+
+    @log_step(logger, "ШАГ 3. Выбор Video Encoder")
+    def select_video_encoder():
+        time.sleep(3.5)
+        wg_page.select_video_encoder(video_encoder_value)
+        base_page.click(wg_page.COMBOBOX_BACK_BUTTON)
+        expected_value = video_encoder_value
+        actual_value = wg_page.get_settings_item_value_text(wg_page.VIDEO_ENCODER_VALUE)
+        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+
+    @log_step(logger, "ШАГ 4. Проверка значения Video Encoder в VT WebGuest Settings")
+    def check_video_encoder_field_value_vt():
+        desktop_app_page.right_click_vt_source_item(vt_web_guest_source_name)
+        desktop_app_page.click_vt_source_item(DesktopAppPage.VT_WEB_GUEST_SETTINGS)
+        desktop_app_page.select_combobox_item_by_index(5, 1)
+        desktop_app_page.click_button_by_name(DesktopAppPage.VT_OK_BUTTON)
+
+    @log_step(logger, "ШАГ 5. Проверка значения поля Video Encoder")
+    def check_video_encoder_field_value():
+        expected_value = video_encoder_value
+        actual_value = wg_page.get_settings_item_value_text(wg_page.VIDEO_ENCODER_VALUE)
+        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+
+    @log_step(logger, "ШАГ 6. Проверка Video Encoder в WebRTC")
+    def check_video_encoder_webrtc():
+        expected_value = webrtc_video_encoder_for_h264
+        codec = stream_handler.get_current_video_codec_from_stats()
+        actual_value = codec['outbound']
+        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+
+    try:
+        check_settings_button()
+        click_settings_button()
+        select_video_encoder()
+        check_video_encoder_field_value_vt()
+        check_video_encoder_field_value()
+        check_video_encoder_webrtc()
+
+    except (NoSuchElementException, TimeoutException) as e:
+
+        logger.error(f"Ошибка при выполнении теста: {e}")
+
+        pytest.fail(f"Ошибка при выполнении теста: {e}")
+
+    # При необходимости закрыть VT
+    # finally:
+
+    # desktop_app.close_application()
