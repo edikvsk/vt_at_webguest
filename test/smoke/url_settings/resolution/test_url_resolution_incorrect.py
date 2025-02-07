24d231d7dd6f6c0dcbd65fd75232aa9ae5ac62d5
Edwardv <edwardv@medialooks.com>
2025-02-07 12:40:19 +0200

    edit test_url_resolution

--- test/smoke/url_settings/resolution/test_url_resolution_incorrect.py
+++ test/smoke/url_settings/resolution/test_url_resolution_incorrect.py
@@ -1,20 +1,16 @@
 import configparser
 import os
-import time
 
 import pytest
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 from pages.base_page import BasePage
-from pages.desktop_app_page import DesktopAppPage
 from pages.web_guest_page import WebGuestPage
-from utils.config import CONFIG_INI, PROCESS_PATH
+from utils.config import CONFIG_INI
 from utils.conftest import driver, modified_fixture
-from utils.desktop_app import DesktopApp
 from utils.helpers import log_step
 from utils.logger_config import setup_logger
 from utils.notificaton_handler import NotificationHandler
-from utils.webrtc_stream_handler import StreamHandler
 
 
 @pytest.fixture(scope="function")
@@ -33,10 +29,7 @@ def test_url_resolution_incorrect(driver, logger):
 
     wg_page = WebGuestPage(driver)
     base_page = BasePage(driver)
-    stream_handler = StreamHandler(driver)
     notification_handler = NotificationHandler(driver, wg_page.NOTIFICATION_ELEMENT, logger)
-    desktop_app = DesktopApp(PROCESS_PATH)
-    desktop_app_page = DesktopAppPage(desktop_app.main_window)
 
     @log_step(logger, "Проверка URL")
     def check_url(drv):
@@ -48,75 +41,18 @@ def test_url_resolution_incorrect(driver, logger):
 
         assert current_url == expected_url, f"Ожидался URL: {expected_url}, но был: {current_url}"
 
-    vt_web_guest_source_name = "Web Guest"
-    resolution = "320X240"
-
     @log_step(logger, "Проверка Notifications")
     def check_notifications():
         assert not notification_handler.check_notification(), "Найдено блокирующее уведомление"
 
     @log_step(logger, "Проверка отображения Authorization Form")
     def check_authorization_form():
-        assert base_page.is_element_visible(wg_page.AUTHORIZATION_FORM), "Authorization Form не отображается"
-
-    @log_step(logger, "Логин")
-    def login():
-        base_page.click(wg_page.LOGIN_BUTTON)
-        assert base_page.is_element_visible(wg_page.STOP_BUTTON), "Логин НЕ выполнен"
-
-    @log_step(logger, "Проверка отображения кнопки SETTINGS")
-    def check_settings_button():
-        assert base_page.is_element_present(wg_page.SETTINGS_BUTTON), "Кнопка SETTINGS не отображается"
-
-    @log_step(logger, "Нажатие кнопки SETTINGS")
-    def click_settings_button():
-        base_page.click(wg_page.SETTINGS_BUTTON)
-        assert wg_page.is_element_visible(wg_page.WG_SETTINGS_WINDOW), "Settings не открыты"
-
-    @log_step(logger, "Проверка значения поля Resolution")
-    def check_resolution_settings():
-        expected_value = resolution
-        actual_value = wg_page.get_settings_item_value_text(wg_page.RESOLUTION_VALUE)
-        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
-
-    @log_step(logger, "Проверка значения Resolution в VT WebGuest Settings")
-    def check_resolution_field_value_vt():
-        desktop_app_page.right_click_vt_source_item(vt_web_guest_source_name)
-        desktop_app_page.click_vt_source_item(DesktopAppPage.VT_WEB_GUEST_SETTINGS)
-        desktop_app_page.select_combobox_item_by_index(0, 0)
-        desktop_app_page.click_button_by_name(DesktopAppPage.VT_OK_BUTTON)
-
-    @log_step(logger, "Проверка значения поля Resolution")
-    def check_resolution_field_value():
-        expected_value = resolution
-        actual_value = wg_page.get_settings_item_value_text(wg_page.RESOLUTION_VALUE)
-        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
-
-    @log_step(logger, "Перезапуск стрима для обновления WebRTC stats")
-    def restart_streaming():
-        base_page.click(wg_page.STOP_BUTTON)
-        base_page.click(wg_page.START_BUTTON)
-        assert wg_page.is_button_pressed(wg_page.STOP_BUTTON), "Кнопка STOP не отображается"
-
-    @log_step(logger, "Проверка значения Resolution в WebRTC")
-    def check_webrtc_frame_dimensions():
-        time.sleep(25)
-        expected_value = resolution
-        actual_value = stream_handler.get_video_frame_dimensions()
-        assert actual_value == expected_value, f"Ожидалось значение '{expected_value}', но получено '{actual_value}'"
+        assert not base_page.is_element_visible(wg_page.AUTHORIZATION_FORM), "Authorization Form отображается"
 
     try:
         check_url(driver)
         check_notifications()
         check_authorization_form()
-        login()
-        check_settings_button()
-        click_settings_button()
-        check_resolution_settings()
-        check_resolution_field_value_vt()
-        check_resolution_field_value()
-        restart_streaming()
-        check_webrtc_frame_dimensions()
 
     except (NoSuchElementException, TimeoutException) as e:
         logger.error(f"Ошибка при выполнении теста: {e}")
